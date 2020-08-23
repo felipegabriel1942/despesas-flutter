20c39823d21a6e0a74124b1885f3c16126a9f9d7
Felipe Gabriel Pinheiro <pinheiro_felipeg@yahoo.com.br>
2020-08-23 19:42:18 -0300

    Criando formulario

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:expenses/models/transaction.dart';
+import 'package:intl/intl.dart';
 
 main() => runApp(ExpensesApp());
 
@@ -13,6 +14,9 @@ class ExpensesApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final titleController = TextEditingController();
+  final valueController = TextEditingController();
+
   final _transactions = [
     Transaction(
         id: 't1',
@@ -30,7 +34,6 @@ class MyHomePage extends StatelessWidget {
           title: Text('Despesas Pessoais'),
         ),
         body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             Container(
@@ -39,9 +42,6 @@ class MyHomePage extends StatelessWidget {
                 child: Text('Gráfico'),
                 elevation: 5,
               ),
-            ),
-            Card(
-              child: Text('Lista de Transações'),
             ),
             Column(
               children: _transactions.map((tr) {
@@ -52,20 +52,62 @@ class MyHomePage extends StatelessWidget {
                         margin:
                             EdgeInsets.symmetric(horizontal: 15, vertical: 10),
                         decoration: BoxDecoration(
-                            border: Border.all(color: Colors.black, width: 2)),
+                            border: Border.all(color: Colors.purple, width: 2)),
                         padding: EdgeInsets.all(10),
-                        child: Text(tr.value.toString()),
+                        child: Text(
+                          'R\$ ${tr.value.toStringAsFixed(2)}',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Colors.purple),
+                        ),
                       ),
                       Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: <Widget>[
-                          Text(tr.title),
-                          Text(tr.date.toString())
+                          Text(
+                            tr.title,
+                            style: TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            DateFormat('d MMM y').format(tr.date),
+                            style: TextStyle(color: Colors.grey),
+                          )
                         ],
                       )
                     ],
                   ),
                 );
               }).toList(),
+            ),
+            Card(
+              elevation: 5,
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  children: [
+                    TextField(
+                      onChanged: (newValue) => titleController.text = newValue,
+                      decoration: InputDecoration(labelText: 'Título'),
+                    ),
+                    TextField(
+                      onChanged: (newValue) => valueController.text = newValue,
+                      decoration: InputDecoration(labelText: 'Valor (R\$)'),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        FlatButton(
+                          child: Text('Nova Transação'),
+                          textColor: Colors.purple,
+                          onPressed: () {},
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
             )
           ],
         ));
